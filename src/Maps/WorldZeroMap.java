33ae736202f85543a9eba4b3b1762dcc3b5d31d1
Juliet Lombardi <juliet@Juliets-MBP.quinnipiac.edu>
2023-10-24 16:19:42 -0400

    internet explorer

--- src/Maps/WorldZeroMap.java
+++ src/Maps/WorldZeroMap.java
@@ -10,11 +10,12 @@ import Level.Script;
 import Level.Trigger;
 import Scripts.SimpleTextScript;
 import Tilesets.CommonTileset;
+import Tilesets.InternetExplorerTileset;
 
 public class WorldZeroMap extends Map {
 
     public WorldZeroMap() {
-        super("world_zero_map.txt", new CommonTileset());
+        super("world_zero_map.txt", new InternetExplorerTileset());
         this.playerStartPosition = getMapTile(0, 0).getLocation();
         
     }
