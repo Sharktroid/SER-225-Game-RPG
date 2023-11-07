2bc9cc5ded5fcdaf15bb392b9ae9c954f1bee0c4
shanbrick <sbrickett27@gmail.com>
2023-11-07 17:36:57 -0500

    added dialogue to OLJ and NSE

--- src/Scripts/WorldOneMap/CatsuitScript.java
+++ src/Scripts/WorldOneMap/CatsuitScript.java
@@ -8,21 +8,60 @@ import Level.Textbox.Style;
 // script for talking to catsuit npc
 public class CatsuitScript extends Script<NPC> {
 
+    private int sequence = 0;
+    // private int miniSequence = 0;
+    private int responseNum = -1;
+
     @Override
     protected void setup() {
         lockPlayer();
 
         setTextboxStyle(Style.WORLDONE);
-        setNPCName("Police Officer");
+        String npcName = "Network Security Engineer";
+        String playerName = "T";
+        // setNPCName("Network Security Engineer");
         showTextbox();
 
-        if (!isFlagSet("hasTalkedToCatsuit")) {
-            addTextToTextboxQueue( "Hello fellow cat");
-            addTextToTextboxQueue( "looks like not everyone likes to be presentable");
-            addTextToTextboxQueue( "talk to me when you get some clothes first");
-        }
-        else {
-            addTextToTextboxQueue( "talk to me when you get some clothes first");
+        String[] selections = {"Sure!", "No way!"};
+        String[] answers = {"Thank you so much. I would do it but I have to tend to my partner here...", "Guess you aren't getting your orb piece..."};
+
+        if (!isFlagSet("hasTalkedToNSE")) {
+            if (sequence == 0) {
+                setNPCName(npcName);
+                addTextToTextboxQueue("Who are you? How did you get here?");
+            } else if (sequence == 1) {
+                setNPCName(playerName);
+                addTextToTextboxQueue("I'm an IT worker from Firefox. I came through a portal.");
+                addTextToTextboxQueue("I'm looking for a purple orb");
+            } else if (sequence == 2) {
+                setNPCName(npcName);
+                addTextToTextboxQueue("Purple orb? I think I found a part of that.");
+                addTextToTextboxQueue("I could give it to you right now but I'm a little occupied...");
+                addTextToTextboxQueue("There seems to be an infection that got in through our\nnetwork. They got my partner...");
+            } else if (sequence == 3) {
+                addTextToTextboxQueue("I know you're not network security, but could you help\nus out?", selections, answers);
+            } else if (sequence == 4) {
+                if (responseNum == 1) {
+                    setNPCName(playerName);
+                    addTextToTextboxQueue("Okay fine! I'll do it!");
+                }
+                setNPCName(npcName);
+                addTextToTextboxQueue("Here's the file I've made on the security breach so far.");
+                addTextToTextboxQueue("The viruses aren't apparent at first, so you'll have to use this file to check if someone is infected.");
+                addTextToTextboxQueue("Also, here's some security tools to fight against the virus.");
+                addTextToTextboxQueue("Come back to me once you've saved everyone.");
+            }
+        } else if (isFlagSet("hasTalkedToNSE") && !isFlagSet("hasCuredAllNPCs")) {
+            setNPCName(npcName);
+            addTextToTextboxQueue("Hurry. You can still save everyone if you act quickly.");
+        } else if (isFlagSet("hasCuredAllNPCs")) {
+            setNPCName(npcName);
+            addTextToTextboxQueue("You removed the virus from everyone? You saved our world, T.");
+            addTextToTextboxQueue("Here is the shard you needed.");
+            addTextToTextboxQueue("I've heard around that another shard crashed through the roof of the library when that earthquake happened.");
+            addTextToTextboxQueue("Maybe go speak to the librarian about it.");
+        } else {
+            addTextToTextboxQueue("Thank you again for saving our world.");
         }
         entity.facePlayer(player);
     }
@@ -32,17 +71,70 @@ public class CatsuitScript extends Script<NPC> {
         unlockPlayer();
         hideTextbox();
 
-        // set flag so that if Sloth is talked to again after the first time, what he says changes
-        setFlag("hasTalkedToCatsuit");
+        if (!isFlagSet("hasTalkedToNSE")) {
+            if (sequence == 0) {
+                sequence++;
+            } else if (sequence == 1) {
+                sequence++;
+            } else if (sequence == 2) {
+                sequence++;
+            } else if (sequence == 3) {
+                responseNum = getChoice();
+                sequence++;
+            } else if (sequence == 4) {
+                setFlag("hasTalkedToNSE");
+                sequence++;
+            }
+        }
     }
 
     @Override
     public ScriptState execute() {
-        start();
-        if (!isTextboxQueueEmpty()) {
+        if (!isFlagSet("hasTalkedToNSE")) {
+            if (sequence == 0) {
+                start();
+                if (isTextboxQueueEmpty()) {
+                    end();
+                }
+            } else if (sequence == 1) {
+                start();
+                if (isTextboxQueueEmpty()) {
+                    end();
+                }
+            } else if (sequence == 2) {
+                start();
+                if (isTextboxQueueEmpty()) {
+                    end();
+                }
+            } else if (sequence == 3) {
+                start();
+                if (isTextboxQueueEmpty()) {
+                    end();
+                }
+            } else if (sequence == 4) {
+                start();
+                if (isTextboxQueueEmpty()) {
+                    end();
+                    return ScriptState.COMPLETED;
+                }
+            }
+            return ScriptState.RUNNING;
+        } else if (isFlagSet("hasTalkedToNSE") && !isFlagSet("hasCuredAllNPCs")) {
+            start();
+            if (isTextboxQueueEmpty()) {
+                end();
+                return ScriptState.COMPLETED;
+
+            }
+            return ScriptState.RUNNING;
+        } else if (isFlagSet("hasCuredAllNPCs")) {
+            start();
+            if (isTextboxQueueEmpty()) {
+                end();
+                return ScriptState.COMPLETED;
+            }
             return ScriptState.RUNNING;
         }
-        end();
         return ScriptState.COMPLETED;
     }
 }
