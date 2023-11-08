5890c9ca36a8252e59cc2b92db6c0de398de10b7
shanbrick <sbrickett27@gmail.com>
2023-11-08 05:03:16 -0500

    finished olj, nse, librarian

--- src/Maps/WorldOneMap.java
+++ src/Maps/WorldOneMap.java
@@ -11,9 +11,7 @@ import NPCs.Beaver;
 import NPCs.Catsuit;
 import NPCs.Dinosaur;
 import NPCs.Elder;
-import NPCs.Garfunkle;
 import NPCs.Giraffe;
-import NPCs.Officeworker;
 import NPCs.SoulConsumingFlameNPC;
 import NPCs.Redpanda;
 import NPCs.Sloth;
@@ -21,16 +19,13 @@ import NPCs.Walrus;
 import Scripts.WorldOneMap.BeaverScript;
 import Scripts.WorldOneMap.CatsuitScript;
 import Scripts.WorldOneMap.DinoScript;
-import Scripts.WorldOneMap.ElderScript;
-import Scripts.WorldOneMap.GarfunkleScript;
 import Scripts.WorldOneMap.GiraffeScript;
-import Scripts.WorldOneMap.OfficeworkerScript;
+import Scripts.WorldOneMap.OldManJenksScript;
 import Scripts.WorldOneMap.RedpandaScript;
 import Scripts.WorldOneMap.SlothScript;
 import Scripts.WorldOneMap.SoulConsumingFlameNPCScript;
 import Scripts.WorldOneMap.WalrusScript;
 import Scripts.WorldOneMap.WorldOneClearScript;
-import Tilesets.CommonTileset;
 import Tilesets.InternetExplorerTileset;
 
 import java.util.ArrayList;
@@ -41,7 +36,7 @@ public class WorldOneMap extends Map {
 
     public WorldOneMap() {
         super("world_one_map.txt", new InternetExplorerTileset());
-        this.playerStartPosition = getMapTile(0, 14).getLocation();
+        this.playerStartPosition = getMapTile(23, 13).getLocation();
     }
 
     @Override
@@ -67,46 +62,42 @@ public class WorldOneMap extends Map {
         ArrayList<NPC> npcs = new ArrayList<>();
         // remember to import NPC and Scripts for new NPC
 
-        Walrus walrus = new Walrus(1, getMapTile(4, 28).getLocation().subtractY(40));
+        //old man jenkins
+        Elder elder = new Elder(9, getMapTile(25, 14).getLocation());
+        elder.setInteractScript(new OldManJenksScript());
+        npcs.add(elder);
+
+        //engineer
+        Catsuit catsuit = new Catsuit(7, getMapTile(35, 24).getLocation());
+        catsuit.setInteractScript(new CatsuitScript());
+        npcs.add(catsuit);
+
+        //librarian
+        Walrus walrus = new Walrus(1, getMapTile(36, 9).getLocation());
         walrus.setInteractScript(new WalrusScript());
         npcs.add(walrus);
 
-        Dinosaur dinosaur = new Dinosaur(2, getMapTile(13, 4).getLocation());
+        //infected npcs
+        Dinosaur dinosaur = new Dinosaur(2, getMapTile(8, 15).getLocation());
         dinosaur.setExistenceFlag("hasTalkedToDinosaur");
         dinosaur.setInteractScript(new DinoScript());
         npcs.add(dinosaur);
 
-        Beaver beaver = new Beaver(3, getMapTile(10, 20).getLocation().subtractY(40));
+        Beaver beaver = new Beaver(3, getMapTile(15, 7).getLocation());
         beaver.setInteractScript(new BeaverScript());
         npcs.add(beaver);
 
-        Giraffe giraffe = new Giraffe(4, getMapTile(11, 21).getLocation().subtractY(40));
+        Giraffe giraffe = new Giraffe(4, getMapTile(3, 8).getLocation());
         giraffe.setInteractScript(new GiraffeScript());
         npcs.add(giraffe);
 
-        Redpanda redpanda = new Redpanda(5, getMapTile(17, 22).getLocation().subtractY(40));
+        Redpanda redpanda = new Redpanda(5, getMapTile(38, 31).getLocation());
         redpanda.setInteractScript(new RedpandaScript());
         npcs.add(redpanda);
 
-        Sloth sloth = new Sloth(6, getMapTile(9, 18).getLocation().subtractY(40));
+        Sloth sloth = new Sloth(6, getMapTile(9, 28).getLocation());
         sloth.setInteractScript(new SlothScript());
         npcs.add(sloth);
-
-        Catsuit catsuit = new Catsuit(7, getMapTile(13, 20).getLocation().subtractY(40));
-        catsuit.setInteractScript(new CatsuitScript());
-        npcs.add(catsuit);
-
-        Officeworker officeworker = new Officeworker(8, getMapTile(11, 16).getLocation().subtractY(40));
-        officeworker.setInteractScript(new OfficeworkerScript());
-        npcs.add(officeworker);
-
-        Elder elder = new Elder(9, getMapTile(14, 20).getLocation().subtractY(40));
-        elder.setInteractScript(new ElderScript());
-        npcs.add(elder);
-
-        Garfunkle garfunkle = new Garfunkle(10, getMapTile(13, 20).getLocation().subtractY(40));
-        garfunkle.setInteractScript(new GarfunkleScript());
-        npcs.add(garfunkle);
 
         SoulConsumingFlameNPC soulConsumingFlame = new SoulConsumingFlameNPC(8, getMapTile(11, 16).getLocation().subtractY(40));
         soulConsumingFlame.setInteractScript(new SoulConsumingFlameNPCScript());
--- src/Scripts/WorldOneMap/WalrusScript.java
+++ src/Scripts/WorldOneMap/WalrusScript.java
@@ -9,38 +9,55 @@ import Level.Textbox.Style;
 public class WalrusScript extends Script<NPC> {
 
     private int sequence = 0;
-    private int responseNum = -1;
+    // private int responseNum = -1;
 
     @Override
     protected void setup() {
         lockPlayer();
 
         setTextboxStyle(Style.WORLDONE);
-        setNPCName("Walrus");
         showTextbox();
 
-        String[] selections = {"What is this place?", "Who are you?", "What are you doing?"};
-        String[] answers = {"This is my response to A", "This is my response to B", "This is my response to C"};
+        String npcName = "Librarian";
+        String playerName = "T";
+
+        String[] selections = {"RUN VIRUS SCAN", "LEAVE"};
+        String[] answers = {"NO VIRUS DETECTED", "Please come again once the library is back in top shape!"};
 
         entity.facePlayer(player);
-        if (!isFlagSet("hasTalkedToWalrus")) {
+        if (isFlagSet("hasRanVirusScanLibrarian")) {
+            addTextToTextboxQueue( "Please come back once everything is fixed.");
+        } else if (!isFlagSet("hasCuredAllNPCs")) {
+            setNPCName(npcName);
+            addTextToTextboxQueue("Sorry, the library is currently closed right now due to\ntraumatic events.", selections, answers);
+        } else if (!isFlagSet("hasTalkedToLibrarian") && isFlagSet("hasCuredAllNPCs")) {
             if (sequence == 0) {
-                addTextToTextboxQueue( "Hi Cat! How can I help you today?", selections, answers);
-                responseNum = getChoice();
+                setNPCName(npcName);
+                addTextToTextboxQueue("Hello!");
             } else if (sequence == 1) {
-                if (responseNum == 0) {
-                    addTextToTextboxQueue( "This is the branch of A.");
-                    addTextToTextboxQueue( "What a wonderful branch.");
-                } else if (responseNum == 1) {
-                    addTextToTextboxQueue( "This is the branch of B.");
-                    addTextToTextboxQueue( "Not too fond of this branch...");
-                } else if (responseNum == 2) {
-                    addTextToTextboxQueue( "This is the branch of C.");
-                    addTextToTextboxQueue( "This branch is alright.");
-                }
+                setNPCName(playerName);
+                addTextToTextboxQueue("I heard something about an object going through the\nroof?");
+            } else if (sequence == 2) {
+                setNPCName(npcName);
+                addTextToTextboxQueue("Yes! An earthquake struck so I ran out of the building.");
+                addTextToTextboxQueue("Then, a purple object flew through the second floor\nwindows!");
+                addTextToTextboxQueue("I haven't gone back inside out of fear...");
+            } else if (sequence == 3) {
+                setNPCName(playerName);
+                addTextToTextboxQueue("I need that object. Do you mind if I go inside?");
+            } else if (sequence == 4) {
+                setNPCName(npcName);
+                addTextToTextboxQueue("Go right ahead!");
             }
+            //set flag
+        } else if (isFlagSet("hasTalkedToLibrarian")) {
+            setNPCName(npcName);
+            addTextToTextboxQueue("How are we going to afford these repairs...");
+        } else if (isFlagSet("hasFoundLibraryShard")) {
+            setNPCName(npcName);
+            addTextToTextboxQueue("You were able to find it and safely remove it? Good job!");
         } else {
-            addTextToTextboxQueue( "I sure love doing walrus things!");
+            addTextToTextboxQueue("Please come again once the library is more presentable.");
         }
     }
 
@@ -50,19 +67,42 @@ public class WalrusScript extends Script<NPC> {
         unlockPlayer();
         hideTextbox();
 
-        if (sequence == 0) {
-            responseNum = getChoice();
-            sequence++;
-        } else if (sequence == 1) {
-            setFlag("hasTalkedToWalrus");
-            sequence++;
+        if (!isFlagSet("hasCuredAllNPCs")) {
+            setFlag("hasRanVirusScanLibrarian");
+        }
+        if (!isFlagSet("hasTalkedToLibrarian")) {
+            if (sequence == 0) {
+                sequence++;
+            } else if (sequence == 1) {
+                sequence++;
+            } else if (sequence == 2) {
+                sequence++;
+            } else if (sequence == 3) {
+                sequence++;
+            } else if (sequence == 4) {
+                setFlag("hasTalkedToLibrarian");
+                sequence++;
+            }
         }
     }
 
     @Override
     public ScriptState execute() {
-        // setTextboxStyle(Textbox.Style.WORLDONE);
-        if (!isFlagSet("hasTalkedToWalrus")) {
+        if (!isFlagSet("hasCuredAllNPCs")) {
+            start();
+            if (isTextboxQueueEmpty()) {
+                end();
+                return ScriptState.COMPLETED;
+            }
+            return ScriptState.RUNNING;            
+        } else if (isFlagSet("hasRanVirusScanLibrarian")) {
+            start();
+            if (!isTextboxQueueEmpty()) {
+                return ScriptState.RUNNING;
+            }
+            end();
+            return ScriptState.COMPLETED;
+        } else if (!isFlagSet("hasTalkedToLibrarian") && isFlagSet("hasCuredAllNPCs")) {
             if (sequence == 0) {
                 start();
                 if (isTextboxQueueEmpty()) {
@@ -72,18 +112,47 @@ public class WalrusScript extends Script<NPC> {
                 start();
                 if (isTextboxQueueEmpty()) {
                     end();
+                }
+            } else if (sequence == 2) {
+                start();
+                if (isTextboxQueueEmpty()) {
+                    end();
+                }
+            } else if (sequence == 3) {
+                start();
+                if (isTextboxQueueEmpty()) {
+                    end();
+                }
+            } else if (sequence == 4) {
+                start();
+                if (isTextboxQueueEmpty()) {
+                    end();
                     return ScriptState.COMPLETED;
                 }
+            } 
+            return ScriptState.RUNNING;
+        } else if (isFlagSet("hasTalkedToLibrarian")) {
+            start();
+            if (isTextboxQueueEmpty()) {
+                end();
+                return ScriptState.COMPLETED;
             }
             return ScriptState.RUNNING;
-        } else if (isFlagSet("hasTalkedToWalrus")) {
+        } else if (isFlagSet("hasFoundLibraryShard")) {
             start();
-            if (!isTextboxQueueEmpty()) {
-                return ScriptState.RUNNING;
+            if (isTextboxQueueEmpty()) {
+                end();
+                return ScriptState.COMPLETED;
             }
-            end();
-            return ScriptState.COMPLETED;
+            return ScriptState.RUNNING;
+        } else {
+            start();
+            if (isTextboxQueueEmpty()) {
+                end();
+                return ScriptState.COMPLETED;
+            }
+            return ScriptState.RUNNING;
         }
-        return ScriptState.COMPLETED;
+        // return ScriptState.COMPLETED;
     }
 }
