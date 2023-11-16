37c93a8fb944478fcc12c14b83325dbe18a4a1bf
shanbrick <sbrickett27@gmail.com>
2023-11-16 16:44:31 -0500

    added image and deleted unneeded files

--- src/Maps/ShannonTestMap.java
+++ src/Maps/ShannonTestMap.java
@@ -4,22 +4,6 @@ import java.util.ArrayList;
 
 import Level.Map;
 import Level.NPC;
-// import NPCs.Beaver;
-// import NPCs.Catsuit;
-// import NPCs.Dinosaur;
-// import NPCs.Elder;
-// import NPCs.Giraffe;
-// import NPCs.Redpanda;
-// import NPCs.Sloth;
-// import NPCs.Walrus;
-// import Scripts.WorldOneMap.BeaverScript;
-// import Scripts.WorldOneMap.EngineerScript;
-// import Scripts.WorldOneMap.DinoScript;
-// import Scripts.WorldOneMap.GiraffeScript;
-// import Scripts.WorldOneMap.OldManJenksScript;
-// import Scripts.WorldOneMap.RedpandaScript;
-// import Scripts.WorldOneMap.SlothScript;
-// import Scripts.WorldOneMap.LibrarianScript;
 import Tilesets.InternetExplorerTileset;
 
 //fastest way to load: start program -> press up arrow -> select hub world -> press 7 key
